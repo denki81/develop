a7c5835f755644e6ca391da41e91078f428a1ad7
denki81 <soe0801@gmail.com>
2018-05-20 03:05:59 +0900

    迫真記法による手触り感の向上

--- 180510/Assets/script/MovingCube.cs
+++ 180510/Assets/script/MovingCube.cs
@@ -18,13 +18,30 @@ public class MovingCube : MonoBehaviour
 	{
 		//joystickを動かすためにvector3座標をpos変数として取り出すぞ
         Vector3 pos = transform.position;
-        //ジョイスティックの入力座標をcontroll変数として取り出すぞ
+        //ジョイスティックの入力座標をcont変数として取り出すぞ
         //この変数は必ず0に戻るから取り合いは注意だぞ
 		Vector3 cont = _joystick.Position;
 
-		//movementに移動量を入れるぞ
-		Vector3 movement = cont - pos;
-
+		//移動スピードがガバガバなのでなんとかするぞ
+		//contのxyが0以上の時に、move変数のxyに1を入れるぞ
+		Vector3 move = Vector3.zero;
+			if (cont.x > 1)
+        		{ 
+        			cont.x = 1; 
+        		}
+    		if (cont.y > 1)
+                {
+                    cont.y = 1;
+                }
+    		if (cont.x < -1)
+                {
+                    cont.x = -1;
+                }
+            if (cont.y < -1)
+                {
+                    cont.y = -1;
+                }
+        
         //joystickの操作が行われたか（contの値が0か）どうか判定して移動させるぞ
 		if (cont != Vector3.zero)
 		{
@@ -36,7 +53,7 @@ public class MovingCube : MonoBehaviour
             //Vector3座標にposを代入
             transform.position = pos;
 
-			//Unityのアニメーターに_joystickのx(y)をDirectionのx(y)として返す
+			//Unityのアニメーターにcontのx(y)をDirectionのx(y)として返す
 			GetComponent<Animator>().SetFloat("Direction_X", cont.x);
 			GetComponent<Animator>().SetFloat("Direction_Y", cont.y);
 
